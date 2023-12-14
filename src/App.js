ed36e6513ffd58e7efe36bb92021d5abade80f70
jrdnspam <jspampin@gmail.com>
2023-12-14 13:24:07 -0500

    added table function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./Home";
 
-function App() {
+import "./App.css";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout>
+        <Switch>
+          <Route path="" exact component={Home} />
+          <Route render={() => <h1>404 Not found</h1>} />
+        </Switch>
+      </Layout>
+    </Router>
   );
-}
+};
 
 export default App;
